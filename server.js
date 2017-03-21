87c95decd04c194e532ea4aa513fa62b4f7c78fa
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-03-21 11:55:42 -0600

    First attempt at get and post requests - with tests

--- server.js
+++ server.js
@@ -1,17 +1,51 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'Secret Box'
+app.locals.secrets = {
+  wow: "I am a cat"
+}
+app.use(bodyParser.json())
+
+// As another example of how we might use bodyParser:
+app.use(bodyParser.urlencoded({extended: true}))
+// This would parse data sent TO the app via html forms
 
 app.get('/', (request, response) => {
-  response.send('It\'s a secret to everyone.')
+  response.send(app.locals.title)
+})
+
+app.get('/api/secrets/:id', (request, response) => {
+  const id = request.params.id
+  const message = app.locals.secrets[id]
+
+  if (!message) { return response.sendStatus(404)  }
+
+  response.json({
+    id, message
+  })
+})
+
+app.post('/api/secrets', (request, response) => {
+  const id = Date.now()
+  const message = request.body.message
+  if (!message) {
+    return response.status(422).send({
+      error: "No message property provided."
+    })
+  }
+  app.locals.secrets[id] = message
+  response.status(201).json({
+    id, message
+  })
 })
 
 // The !module.parent line is checking if the server was launched from
 // the command line vs. started from a test.  The below case is what to
 // do when the server is launched from the command line. Test setup will 
-// be separate.
+// be separate.  Rails does something similar.
 
 if (!module.parent) {
   app.listen(app.get('port'), () => {
--- test/server-test.js
+++ test/server-test.js
@@ -24,11 +24,69 @@ describe('Server', () => {
     assert(app)
   })
 
+  describe('GET api/secrets/:id', () => {
+    beforeEach(() => {
+      app.locals.secrets = {
+        horse: "Penelope"
+      }
+    })
+
+    it('returns 404 when resource not found', (done) => {
+      this.request.get('/api/secrets/not-there', (error, response) => {
+        if (error) { done(error) }
+        assert.equal(response.statusCode, 404)
+        done()
+      })
+    })
+
+    it('returns the correct data when a valid id is requested', (done) => {
+      this.request.get('/api/secrets/horse', (error, response) => {
+        if (error) { done(error) }
+        assert.equal(response.statusCode, 200)
+        assert.include(response.body, "Penelope")
+        done()
+      })
+    })
+  })
+
   describe('GET /', () => {
     it('should return a 200 status code', (done) => {
       this.request.get('/', (error, response) => {
         if (error) { done(error) }
         assert.equal(response.statusCode, 200)
+        done()
+      })
+    })
+
+    it('returns the app title', (done) => {
+      this.request.get('/', (error, response) => {
+        const title = app.locals.title
+        if (error) { done(error) }
+        assert.equal(response.body, title)
+        done()
+      })
+    })
+  })
+
+  describe('POST /api/secrets', () => {
+    beforeEach(() => {
+      app.locals.secrets = {}
+    })
+
+    it('should not return a 404', (done) => {
+      this.request.post('/api/secrets', (error, response) => {
+        if (error) { done(error) }
+        assert.notEqual(response.statusCode, 404)
+        done()
+      })
+    })
+
+    it('should receive and store data', (done) => {
+      const message = { message: "Horses are pretty" }
+      this.request.post('/api/secrets', { form: message }, (error, response) => {
+        if (error) { done(error) }
+        const secretCount = Object.keys(app.locals.secrets).length
+        assert.equal(secretCount, 1)
         done()
       })
     })
